ce196b5338a1ed040345aa4ddcd56a27d3997530
Dominik Buse <dom@toifras.de>
2013-06-08 17:49:00 +0200

    Steigerungsspaltengrenzen berichtigt, A+ eingeführt und auf dieses und H begrenzt

--- src/util.py
+++ src/util.py
@@ -8,8 +8,14 @@ from __future__ import unicode_literals
 
 def chr_inc(c, inc=1):
     """Gebe den den inc-ten Nachfolger des Buchstabens c zurück"""
-    return chr(ord(c) + inc)
+    if ord(c) + inc > 72:
+        return 'H'
+    else:
+        return chr(ord(c) + inc)
 
 def chr_dec(c, dec=1):
     """Gebe den den dec-ten Vorgänger des Buchstabens c zurück"""
-    return chr(ord(c) - dec)
+    if ord(c) - dec < 65:
+        return "A+"
+    else:
+        return chr(ord(c) - dec)
