d70f6f0ba599b9f7edb36507ab1ac5be64fd2c44
Dominik S. Buse <buse@ccs-labs.org>
2017-01-06 15:05:59 +0100

    add basic setup/distribution files

---
+++ README.rst
@@ -0,0 +1,16 @@
+DSA Heldenblatt Printing Utility -- PyHeldenBlatt
+=================================================
+
+A tool to print custom character sheets for the german pen & paper rpg
+"Das Schwarze Auge" (DSA), a.k.a "The Dark Eye".
+Please note that this tool is kept mostly in german, as all the vocabulary
+adapted from the game is also in german.
+
+
+Heldenblatt-Generator für "Das Schwarze Auge"-Helden.
+-----------------------------------------------------
+
+Mit diesem Werkzeug können angepasste Heldenbogen-PDFs erzeugt werden, die im
+Stil den Original-Bögen entsprechen, allerdings im Inhalt und Layout angepasst
+werden können.
+Weiterhin füllt das Werkzeug Standard
---
+++ setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='pyheldenblatt',
+
+    # TODO: enhance versioning
+    version='0.1.0rc1',
+
+    description='character sheet generator for the p&p rpg "Das Schwarze Auge"',
+    long_description=long_description,
+
+    # TODO: create public project website and add it here
+    url='https://dav.toifras.de/hg/pyheldenblatt',
+
+    author='Dominik S. Buse',
+    author_email='dbuse@toifras.de',
+
+    # Keep in sync with LICENSE.txt
+    license='Apache 2.0',
+
+    # TODO: add classifiers -- audience, topic and version support
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: Apache Software License',
+    ],
+
+    keywords='DSA character sheet generator heldenblatt heldenbogen',
+
+    # FIXME: add all import packages or use find_packages
+    packages=find_packages(exclude=['data', 'helden']),
+
+    install_requires=['fpdf>=1.7.2'],
+
+    # TODO: add dep groups for developement/testing
+    extras_require={},
+
+    # FIXME: add data files here
+    package_data={},
+
+    # TODO: convert cli script into entry point
+    entry_points={},
+)
